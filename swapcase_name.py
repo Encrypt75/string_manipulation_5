f84e1856b5ee9686bbc7f3747e199d2a5054a58a
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-03-24 11:00:31 +0800

    swapcase_name_5
    This program converts all lower case to an upper case and vice versa.

---
+++ swapcase_name.py
@@ -0,0 +1,5 @@
+#asks the user for the full name
+words = input("Enter full name: ")
+
+#print and uses swapcase() to lower the capital letter and upper to small letters and vice versa
+print(words.swapcase())
